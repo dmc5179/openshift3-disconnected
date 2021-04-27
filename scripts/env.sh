8036ac1a750242c4043c0d389ed1acf47097d622
Dan Clark <danclark@redhat.com>
2021-04-27 01:49:19 +0000

    Add env var file to control all scripts

---
+++ scripts/env.sh
@@ -0,0 +1,13 @@
+
+# Version of OpenShift 3 to pull. Must include the v
+OCP_VERSION=v3.11.420
+
+# podman or docker
+RUNTIME=podman
+
+# If sudo is NOT required, set this field to empty
+SUDO=
+
+# Directory to save OpenShift 3 image archives
+LOCATION="/opt/openshift/ocp-${OCP_VERSION}"
+
