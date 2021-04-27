6755959be5f9c46153d9c7c3d700f81dd092d2a7
Dan Clark <danclark@redhat.com>
2021-04-27 02:01:15 +0000

    Add script to enable required repos for mirroring

---
+++ scripts/rpms/enable_repos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.11-rpms" \
+    --enable="rhel-7-server-ansible-2.9-rpms"
